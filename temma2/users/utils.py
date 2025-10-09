a8bea8d9f9edce3ff4892b89f89b69df5a2cacbb
fayyoz24 <fayyozusmon@gmail.com>
2025-10-09 11:40:42 +0500

    added new

--- temma2/users/utils.py
+++ temma2/users/utils.py
@@ -3,15 +3,45 @@ import os
 from twilio.rest import Client
 import json
 from decouple import config
+import requests
+from requests.auth import HTTPBasicAuth
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = config("ACCOUNT_SID")
-auth_token = config("AUTH_TOKEN")
-client = Client(account_sid, auth_token)
+ACCOUNT_SID = config("ACCOUNT_SID")
+AUTH_TOKEN = config("AUTH_TOKEN")
+FROM = config("FROM")
+CONTENT_SID=config("CONTENT_SID")
+
+url = f"https://api.twilio.com/2010-04-01/Accounts/{ACCOUNT_SID}/Messages.json"
 
 def twilio_whatsapp(receiver):
-     message = client.messages.create(
-         from_=f"whatsapp:{config('FROM')}",
-         to=f"whatsapp:{receiver}",
-         content_sid=config("CONTENT_SID"),
-     )
+
+    data = {
+    "To": f"whatsapp:{receiver}",  # recipient
+    "From": "whatsapp:",  # your approved Twilio WhatsApp sender
+    "ContentSid":f"{CONTENT_SID}",  # your template SID
+    }
+
+    response = requests.post(url, data=data, auth=HTTPBasicAuth(ACCOUNT_SID, AUTH_TOKEN))
+    response.raise_for_status()
+    return response.text
+    # print(response.status_code)
+
+
+# print(response.text)
+
+
+
+
+
+
+
+
+
+
+
+    #  message = client.messages.create(
+    #      from_=f"whatsapp:{config('FROM')}",
+    #      to=f"whatsapp:{receiver}",
+    #      content_sid=config("CONTENT_SID"),
+    #  )
